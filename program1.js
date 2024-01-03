36ba4ab271844a939f3fd2aca96eadcd325dec4a
astitva-techdome <astitva.patle@techdome.net.in>
2024-01-03 19:53:41 +0530

    solve first question

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    for (let char of s) {
+        if (mapping[char]) {
+            const topElement = stack.pop() || '#';
+            if (mapping[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    if(stack.length === 0) { 
+        return true;
+    }
+    return false;
 };
 
 module.exports = { isValid };
